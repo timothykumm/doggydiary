e51de2287cb734d29d0233717a58d8f913331b1b
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-05-11 23:47:32 +0200

    Basic structure added

---
+++ backend/src/main/java/de/unternehmenssoftware/doggydiary/web/controller/UserController.java
@@ -0,0 +1,16 @@
+package de.unternehmenssoftware.doggydiary.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1/")
+@RequestMapping(path = "/api/v1")
+public class UserController {
+
+    @GetMapping(path = "/test")
+    public String testReturn() {
+        return "test";
+    }
+
+}

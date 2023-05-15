a292ba36fb3d8996d1e0639ce80a8935eef70bd3
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-05-16 01:26:29 +0200

    Transform Entities and Sur-Forename added

--- backend/src/main/java/de/unternehmenssoftware/doggydiary/web/entity/dao/UserEntity.java
+++ backend/src/main/java/de/unternehmenssoftware/doggydiary/web/entity/dao/UserEntity.java
@@ -1,36 +1,54 @@
 package de.unternehmenssoftware.doggydiary.web.entity.dao;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-
+import de.unternehmenssoftware.doggydiary.web.entity.dto.User;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity(name = "users")
-public class UserEntity {
+@NoArgsConstructor
+public class UserEntity implements UserDetails {
 
     @Id
-    @Column(name = "email", length = 50)
+    @Column(name = "email", length = 100)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "forename", length = 100)
+    private String forename;
+
+    @Column(name = "surname", length = 100)
+    private String surname;
+
+    @Column(name = "password", length = 100)
     private String password;
 
     @OneToMany(mappedBy = "user")
     private List<DogEntity> dogs;
 
-    public UserEntity() {}
+    @Enumerated(EnumType.STRING)
+    private AuthRole authRole;
 
-    public UserEntity(String email, String password) {
+    public UserEntity(String email, String forename, String surname, String password) {
         this.email = email;
+        this.forename = forename;
+        this.surname = surname;
         this.password = password;
+        this.authRole = AuthRole.USER;
     }
 
-    public UserEntity(String email, String password, List<DogEntity> dogs) {
+    public UserEntity(String email, String forename, String surname, String password, List<DogEntity> dogs) {
         this.email = email;
+        this.forename = forename;
+        this.surname = surname;
         this.password = password;
         this.dogs = dogs;
+        this.authRole = AuthRole.USER;
     }
 
     public String getEmail() {
@@ -39,6 +57,22 @@ public class UserEntity {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPassword() {
@@ -56,4 +90,40 @@ public class UserEntity {
     public void setDogs(List<DogEntity> dogs) {
         this.dogs = dogs;
     }
+
+    public User transformToUser() {
+        return new User(email, forename, surname, password, dogs);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<AuthRole> enumList = Arrays.stream(AuthRole.values()).toList();
+        return enumList.stream().map(e -> new SimpleGrantedAuthority(e.name())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
--- backend/src/main/java/de/unternehmenssoftware/doggydiary/web/entity/dto/User.java
+++ backend/src/main/java/de/unternehmenssoftware/doggydiary/web/entity/dto/User.java
@@ -1,26 +1,44 @@
 package de.unternehmenssoftware.doggydiary.web.entity.dto;
 
+import de.unternehmenssoftware.doggydiary.web.entity.dao.AuthRole;
 import de.unternehmenssoftware.doggydiary.web.entity.dao.DogEntity;
-
+import de.unternehmenssoftware.doggydiary.web.entity.dao.UserEntity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 import java.util.List;
 
-public class User {
+@NoArgsConstructor
+public class User implements UserDetails {
 
     private String email;
+    private String forename;
+    private String surname;
     private String password;
     private List<DogEntity> dogs;
 
-    public User() {}
+    @Enumerated(EnumType.STRING)
+    private AuthRole authRole;
 
-    public User(String email, String password) {
+    public User(String email, String forename, String surname, String password) {
         this.email = email;
+        this.forename = forename;
+        this.surname = surname;
         this.password = password;
+        authRole = AuthRole.USER;
     }
 
-    public User(String email, String password, List<DogEntity> dogs) {
+    public User(String email, String forename, String surname, String password, List<DogEntity> dogs) {
         this.email = email;
+        this.forename = forename;
+        this.surname = surname;
         this.password = password;
         this.dogs = dogs;
+        authRole = AuthRole.USER;
     }
 
     public String getEmail() {
@@ -29,6 +47,22 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPassword() {
@@ -45,5 +79,39 @@ public class User {
 
     public void setDogs(List<DogEntity> dogs) {
         this.dogs = dogs;
+    }
+
+    public UserEntity transformToUserEntity() {
+        return new UserEntity(email, forename, surname, password);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(authRole.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }

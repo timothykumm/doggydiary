f04b083c1940af37252a72581e81c3bd2628ae4a
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-05-28 17:48:33 +0200

    Document tests

---
+++ backend/src/test/java/de/unternehmenssoftware/doggydiary/web/repository/DocumentRepositoryTest.java
@@ -0,0 +1,40 @@
+package de.unternehmenssoftware.doggydiary.web.repository;
+
+import de.unternehmenssoftware.doggydiary.web.entity.DocumentEntity;
+import de.unternehmenssoftware.doggydiary.web.entity.DogEntity;
+import de.unternehmenssoftware.doggydiary.web.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DocumentRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    DogRepository dogRepository;
+
+    @Autowired
+    DocumentRepository documentRepository;
+
+    @Test
+    void getDocumentsByUserAndDogId() {
+        final UserEntity user = new UserEntity("saveduser@gmail.com", "Reiner", "Wahnsinn", "geheim");
+        final DogEntity dog = new DogEntity("Mopsi", "Schäferhund", 3, user);
+        final DocumentEntity document = new DocumentEntity("Arztbesuch", "Dem Hund geht es gut", dog);
+
+        userRepository.save(user);
+        DogEntity savedDog = dogRepository.save(dog);
+        documentRepository.save(document);
+
+        List<DocumentEntity> documents = documentRepository.getDocumentsByUserAndDogId(user.getEmail(), savedDog.getId().toString());
+
+        assertEquals(documents.size(), 1);
+    }
+}
---
+++ backend/src/test/java/de/unternehmenssoftware/doggydiary/web/service/DocumentServiceTest.java
@@ -0,0 +1,86 @@
+package de.unternehmenssoftware.doggydiary.web.service;
+
+import de.unternehmenssoftware.doggydiary.web.controller.request.DocumentRequest;
+import de.unternehmenssoftware.doggydiary.web.entity.DocumentEntity;
+import de.unternehmenssoftware.doggydiary.web.entity.DogEntity;
+import de.unternehmenssoftware.doggydiary.web.entity.UserEntity;
+import de.unternehmenssoftware.doggydiary.web.entity.dto.Document;
+import de.unternehmenssoftware.doggydiary.web.entity.dto.User;
+import de.unternehmenssoftware.doggydiary.web.repository.DocumentRepository;
+import de.unternehmenssoftware.doggydiary.web.repository.DogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class DocumentServiceTest {
+
+    @Mock
+    DogRepository dogRepository;
+
+    @Mock
+    DocumentRepository documentRepository;
+
+    @Mock
+    AuthService authService;
+
+    @InjectMocks
+    DocumentService documentService;
+
+    @BeforeEach
+    void setup() {
+         documentService = new DocumentService(authService, documentRepository, dogRepository);
+    }
+
+    @Test
+    void getAllDocumentsByDog() {
+        User user = new User("dasistder@gmail.com", "Reiner", "Wahnsinn");
+        List<DocumentEntity> documentEntities = List.of(
+                new DocumentEntity("Titel1", "Content1", Mockito.mock(DogEntity.class)),
+                new DocumentEntity("Titel2", "Content2", Mockito.mock(DogEntity.class)));
+
+        when(authService.getAuthenticatedUser()).thenReturn(user);
+        when(documentRepository.getDocumentsByUserAndDogId(Mockito.anyString(), Mockito.anyString())).thenReturn(documentEntities);
+
+        Optional<List<Document>> documents = documentService.getAllDocumentsByDog("12345");
+
+        assertEquals(documents.get(), documentEntities.stream().map(DocumentEntity::transformToDocument).toList());
+    }
+
+    @Test
+    void createDocumentSucceeds() {
+        DocumentRequest documentRequest = new DocumentRequest("Title1", "Content1", 12345L);
+        DogEntity dogEntity = new DogEntity("Fiffi", "Mops", 8 , Mockito.mock(UserEntity.class));
+        DocumentEntity documentEntity = new DocumentEntity(documentRequest.title(), documentRequest.content(), dogEntity);
+
+        when(dogRepository.getDogEntityById(Mockito.anyLong())).thenReturn(dogEntity);
+        when(documentRepository.save(Mockito.any(DocumentEntity.class))).thenReturn(documentEntity);
+
+        Optional<Document> actual = documentService.createDocument(documentRequest);
+        assertEquals(actual.get(), documentEntity.transformToDocument());
+    }
+
+    @Test
+    void createDocumentTFails() {
+        DocumentRequest documentRequest = new DocumentRequest("Title1", "Content1", 12345L);
+        DogEntity dogEntity = new DogEntity("Fiffi", "Mops", 8 , Mockito.mock(UserEntity.class));
+
+        when(dogRepository.getDogEntityById(Mockito.anyLong())).thenReturn(dogEntity);
+        when(documentRepository.save(Mockito.any(DocumentEntity.class))).thenThrow(IllegalArgumentException.class);
+
+        Optional<Document> actual = documentService.createDocument(documentRequest);
+        assertEquals(actual, Optional.empty());
+    }
+}

b5cb735b4e4c8789d6eeade1e8fca2587b29ce78
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-06-03 18:10:44 +0200

    Renamed frontend

---
+++ frontend_vue/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <body>
+    <NavigationBar name="DoggyDiary"></NavigationBar>
+    <ScanDocument></ScanDocument>
+  </body>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar.vue';
+import ScanDocument from '@/components/ScanDocument.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavigationBar,
+    ScanDocument
+}
+}
+</script>

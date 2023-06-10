266886a0f1b02f2b40d3b1c829387863a344dbf0
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-06-10 05:42:23 +0200

    routing added

--- frontend_angular/src/app/app-routing.module.ts
+++ frontend_angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DogInnerviewComponent } from './components/dog-innerview/dog-innerview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'dog', component: DogInnerviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

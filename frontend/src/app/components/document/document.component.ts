33e995425114d32b5e6a2c226f411e7c491907a3
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-06-12 20:28:14 +0200

    New document-view and design

--- frontend/src/app/components/document/document.component.ts
+++ frontend/src/app/components/document/document.component.ts
@@ -50,8 +50,17 @@ constructor(private route: ActivatedRoute, private documentService: DocumentServ
     this.documents = [...this.documents, document];
   }
 
-  selectDocument(document: DocumentGetResponse) {
-    console.log(document.content)
+  toggleDocument(document: DocumentGetResponse) {
+    const mediaText = window.document.getElementById("media-text-" + document.id);
+    const mediaBody = window.document.getElementById("media-body-" + document.id);
+    const borderButtom = window.document.getElementById("border-bottom-" + document.id);
+
+    if (mediaText != null && mediaBody != null && borderButtom) {
+      mediaText.style.display === '' ? mediaText.style.display = 'block' : mediaText.style.display = '';
+      mediaBody.style.display === '' ? mediaBody.style.display = 'block' : mediaBody.style.display = '';
+      !borderButtom.classList.contains('border-bottom') ? borderButtom.classList.add('border-bottom') : borderButtom.classList.remove('border-bottom');
+    }
+
   }
 
 }

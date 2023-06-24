4a4f04567a346253dcd6f36598fda9c718dcfc11
Timothy Kumm <timothykumm@users.noreply.github.com>
2023-06-25 01:35:59 +0200

    Refactor tests

--- backend/src/test/java/de/unternehmenssoftware/doggydiary/web/controller/DocumentControllerTest.java
+++ backend/src/test/java/de/unternehmenssoftware/doggydiary/web/controller/DocumentControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
@@ -50,6 +51,51 @@ class DocumentControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(request))
                 .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()));
+    }
+
+    @Test
+    void putDocument() throws Exception {
+
+        //create document
+        DocumentRequest documentRequest = new DocumentRequest("NeuesDokument2", "Zum testen2", TestDataInitializer.dog.getId());
+        String request = objectMapper.writeValueAsString(documentRequest);
+
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post(BASE_BATH)
+                        .header("Authorization", "Bearer " + TestDataInitializer.authToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value())).andReturn();
+
+        //modify document
+        DocumentRequest editedDocumentRequest = new DocumentRequest("NeuesDokument2Edited", "Zum testen2 edited", TestDataInitializer.dog.getId());
+        String editedRequest = objectMapper.writeValueAsString(editedDocumentRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_BATH + "?documentId=" + response.getResponse().getContentAsString())
+                        .header("Authorization", "Bearer " + TestDataInitializer.authToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(editedRequest))
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()));
+    }
+
+    @Test
+    void deleteDocument() throws Exception {
+
+        //create document
+        DocumentRequest documentRequest = new DocumentRequest("NeuesDokument2", "Zum testen2", TestDataInitializer.dog.getId());
+        String request = objectMapper.writeValueAsString(documentRequest);
+
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post(BASE_BATH)
+                        .header("Authorization", "Bearer " + TestDataInitializer.authToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value())).andReturn();
+
+        //delete document
+        mockMvc.perform(MockMvcRequestBuilders.delete(BASE_BATH + "?documentId=" + response.getResponse().getContentAsString() + "&dogId=" + TestDataInitializer.dog.getId())
+                        .header("Authorization", "Bearer " + TestDataInitializer.authToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()));
     }
 
 }
